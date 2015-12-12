b1de127fc314f63c0ddbf1acb2812f94b003d424
Morgan <d274798495@gmail.com>
2015-12-12 21:02:23 +0800

    new place start HYHTableView

---
+++ HYHTableView/Controllers/HYHVC.m
@@ -0,0 +1,37 @@
+//
+//  HYHVC.m
+//  HYHTableView
+//
+//  Created by skyclass on 15/12/8.
+//  Copyright © 2015年 skyclass. All rights reserved.
+//
+
+#import "HYHVC.h"
+
+@interface HYHVC ()
+
+@end
+
+@implementation HYHVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
